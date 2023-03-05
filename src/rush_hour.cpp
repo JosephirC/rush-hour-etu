7c39933c872bab00b85206a33c4f8b3ae0ae36f4
Youssef Abida <youssef.abida@etu.univ-lyon1.fr>
2023-03-05 20:08:18 +0100

    sdl marche

--- src/rush_hour.cpp
+++ src/rush_hour.cpp
@@ -2,6 +2,13 @@
 #include "plateau.hpp"
 #include <iostream>
 
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+
+
 using namespace std;
 
 int main(){

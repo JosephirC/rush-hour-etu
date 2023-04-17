7318ec598489690d1966c2643e5e32b2379260ef
Youssef Abida <youssef.abida@etu.univ-lyon1.fr>
2023-04-17 11:06:56 +0200

    merged all grid generating code

--- src/puzzle.cpp
+++ src/puzzle.cpp
@@ -2,7 +2,6 @@
 #include <cstdlib>
 #include <fstream>
 #include <algorithm>
-#include <ctime>
 
 Puzzle::Puzzle(){
 
@@ -19,15 +18,7 @@ Puzzle::Puzzle(){
     carsDirection = 0;
     isSolvable = false;
 
-    for (int i=0; i<6; i++) {
-        for (int j=0; j<6; j++) {
-            std::string str_i = std::to_string(i);
-            std::string str_j = std::to_string(j);
-            std::string s = str_i + str_j;
-            freePositions.push_back(s);
-        }
-    }
-
+    //grid.initEmptyGrid();
 }
 
 Puzzle::~Puzzle(){
@@ -97,6 +88,101 @@ void Puzzle::makeEmptyGrid(){
     vector<Car> c;
     grid.setCarArray(c);
 
+}
+
+void Puzzle::randCar(int gridCarId[6][6]){ //gridCarId = -1 au debut et on l'update au fur et a mesure
+
+    cout << numberOfCars << " defsgvd" << endl; 
+
+    for(int i = 0; i < numberOfCars; i++){
+
+        int direction = rand()%2;
+        vector<int> line;
+        bool putCar = false;
+
+        if(direction == HORIZONTAL){ // horizontal
+            int x = rand()%6;
+            int y;
+            int yy = 0;
+            int size;
+            while(gridCarId[x][yy] ==-1 && yy < WIDTH){ // cette boucle me permet de stocker les y qui sont vides
+                line.push_back(yy);
+                yy++;
+            }
+
+            //if(line.size() < 3){ // je n'ajoute une voiture que si j'ai au moins 3 cases vides dans la ligne que je teste
+                while(!putCar){
+                    if(!line.empty()){
+                        int n = rand() % line.size(); // je tire un y aleatoire de mon vector
+                        y = line[n];
+                        size = rand() % 2 + 2;
+
+                        if(gridCarId[x][y] == -1 && gridCarId[x][y + size - 1] == -1 && gridCarId[x][y + size] == -1){
+                            car.setPosX(x);
+                            car.setPosY(y);
+                            car.setDirection(direction);
+                            car.setSize(size);
+                            car.setId(i);
+                            grid.addCar(car);
+
+                            grid.setGridCarId(x, y, i);
+                            grid.setGridCarId(x, y + size - 1, i);
+                            grid.setGridCarId(x, y + size, i);
+
+                            line.erase(line.begin() + n);
+                            putCar = true;
+
+                        }
+                        // else{
+                        //     break;
+                        // }
+                    }
+                }
+            //}
+        }
+        else if(direction == VERTICAL){ // horizontal
+            int x;
+            int y = rand()%6;
+            int xx = 0;
+            int size;
+            while(gridCarId[xx][y] ==-1 && xx < HEIGHT){ // cette boucle me permet de stocker les y qui sont vides
+                line.push_back(xx);
+                xx++;
+            }
+
+            //if(line.size() < 3){ // je n'ajoute une voiture que si j'ai au moins 3 cases vides dans la ligne que je teste
+                while(!putCar){
+                    if(!line.empty()){
+                        int n = rand() % line.size(); // je tire un y aleatoire de mon vector
+                        x = line[n];
+                        size = rand() % 2 + 2;
+
+                        if(gridCarId[x][y] == -1 && gridCarId[x + size - 1][y] == -1 && gridCarId[x + size][y] == -1){
+                            car.setPosX(x);
+                            car.setPosY(y);
+                            car.setDirection(direction);
+                            car.setSize(size);
+                            car.setId(i);
+                            grid.addCar(car);
+
+                            grid.setGridCarId(x, y, i);
+                            grid.setGridCarId(x + size - 1, y, i);
+                            grid.setGridCarId(x + size, y, i);
+
+                            line.erase(line.begin() + n);
+                            line.erase(line.begin() + n+size-1);
+                            line.erase(line.begin() + n+size);
+                            putCar = true;
+
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                }
+            //}
+        }
+    }
 }
 
 int Puzzle::randomGrid(int nbCars) {
@@ -202,4 +288,263 @@ Grid Puzzle::generateRandomGrid(int carMin, int carMax){
     grid.displayGridId(); //test ok
 
     return grid;
+}
+
+/*
+
+    grid.setGridCarId(x, y, i);
+    grid.setGridCarId(x, y + size - 1, i);
+    grid.setGridCarId(x, y + size, i);
+
+    grid.setGridCarId(x, y, i);
+    grid.setGridCarId(x + size - 1, y, i);
+    grid.setGridCarId(x + size, y, i);
+
+*/
+
+void Puzzle::randCar2(int gridCarId[6][6]){
+    
+    cout << numberOfCars << " defsgvd" << endl; 
+
+    int count = 0;
+
+    for(int i = 0; i < numberOfCars; i++){
+        int direction = rand()%2;
+        vector<int> line;
+        bool putCar = false;
+        
+        if(direction == HORIZONTAL){
+            int x = rand()%6;
+            int y;
+            int size = rand() % 2 + 2;
+            for (int yy = 0; yy < WIDTH; yy++) { // Check for available spaces in the line
+                if(gridCarId[x][yy] == -1 && gridCarId[x][yy + size - 2] == -1 && gridCarId[x][yy + size-1] == -1
+                && gridCarId[x][yy] < WIDTH && gridCarId[x][yy + size - 2] < WIDTH && gridCarId[x][yy + size-1] < WIDTH){
+                    line.push_back(yy);
+
+                    cout << "direction : " << direction << endl;
+                    cout << "x : " << x << ", y : " << yy << ", k : " << size  << endl;
+                    cout << "Grid value at " << x << ", " << yy  << " : " << gridCarId[x][yy+size] << endl << endl ;
+
+                }
+            }
+
+            if(!line.empty()){ // Check if there are any valid spaces in the line
+                int n = rand() % line.size();
+                y = line[n];
+                
+
+                //cout << "x : " << x << " y : " << y << endl;
+
+                car.setPosX(x);
+                car.setPosY(y);
+                car.setDirection(direction);
+                car.setSize(size);
+                car.setId(i);
+                grid.addCar(car);
+                count++;
+
+                // for (int j = 0; j < size; j++) { // Mark the car's cells in the gridCarId array
+                //     gridCarId[x][y+j] = i;
+                // }
+
+                grid.setGridCarId(x, y, i);
+                grid.setGridCarId(x, y + size - 2, i);
+                grid.setGridCarId(x, y + size - 1, i);
+
+                // line.erase(line.begin() + n);
+                // line.erase(line.begin() + n+size-2);
+                // line.erase(line.begin() + n+size-1);
+
+                putCar = true;
+            }
+        }
+        else if(direction == VERTICAL){
+            int x;
+            int y = rand()%6;
+            int size = rand() % 2 + 2;
+            for (int xx = 0; xx < HEIGHT; xx++) { // Check for available spaces in the column
+                if(gridCarId[xx][y] == -1 && gridCarId[xx + size -2 ][y] == -1 && gridCarId[xx + size-1][y] == -1
+                && gridCarId[xx][y] < HEIGHT && gridCarId[xx + size -2 ][y] < HEIGHT && gridCarId[xx + size-1][y] < HEIGHT){
+                    line.push_back(xx);
+
+                    cout << "direction : " << direction << endl;
+                    cout << "x : " << xx << ", y : " << y << ", k : " << size  << endl;
+                    cout << "Grid value at " << xx << ", " << y  << " : " << gridCarId[xx + size][y] << endl << endl ;
+
+                }
+            }
+
+            if(!line.empty()){ // Check if there are any valid spaces in the column
+                int n = rand() % line.size();
+                x = line[n];
+                size = rand() % 2 + 2;
+
+                //cout << "x : " << x << " y : " << y << endl;
+
+                car.setPosX(x);
+                car.setPosY(y);
+                car.setDirection(direction);
+                car.setSize(size);
+                car.setId(i);
+                grid.addCar(car);
+
+                count++;
+
+                // for (int j = 0; j < size; j++) { // Mark the car's cells in the gridCarId array
+                //     gridCarId[x+j][y] = i;
+                // }
+
+                grid.setGridCarId(x, y, i);
+                grid.setGridCarId(x + size - 2, y, i);
+                grid.setGridCarId(x + size - 1, y, i);
+
+                // line.erase(line.begin() + n);
+                // line.erase(line.begin() + n+size-2);
+                // line.erase(line.begin() + n+size-1);
+
+                putCar = true;
+            }
+        }
+        if(!putCar){ // If the car was not put, decrement i to try again
+            i--;
+        }
+    }
+    // Check if all the cars have been drawn, and try again if not
+    cout << "dcount" << count << endl;
+    // if(count< numberOfCars){
+    //     cout << " no " << endl;
+    //     randCar2(gridCarId);
+    // }
+}
+
+// void Puzzle::randCar2(int gridCarId[6][6]){
+    
+//     cout << numberOfCars << " defsgvd" << endl; 
+//     int count = 0;
+
+//     for(int i = 0; i < numberOfCars; i++){
+//         int direction = rand()%2;
+//         vector<int> line;
+//         bool putCar = false;
+        
+//         if(direction == HORIZONTAL){
+//             int x = rand()%6;
+//             int y;
+//             int size = rand() % 2 + 2;
+//             for (int yy = 0; yy <= WIDTH; yy++) { // Check for available spaces in the line
+//                 bool validSpace = false;
+//                 for (int k = 0; k < size; k++) {
+
+//                     cout << "x : " << x << ", y : " << yy+k << ", k : " << k  << endl;
+//                     cout << "Grid value at " << x << ", " << yy+k  << " : " << gridCarId[x][yy+k] << endl << endl ;
+
+//                     if (gridCarId[x][yy + k] == -1) {
+//                         validSpace = true;
+//                         break;
+//                     }
+//                 }
+//                 if (!validSpace) {
+//                     line.push_back(yy);
+//                 }
+//             }
+
+//             if(!line.empty()){ // Check if there are any valid spaces in the line
+//                 int n = rand() % line.size();
+//                 y = line[n];
+                
+//                 // Check if car is inside the grid
+//                 if(x >= 0 && x < HEIGHT && y >= 0 && y + size - 1 < WIDTH) {
+//                     car.setPosX(x);
+//                     car.setPosY(y);
+//                     car.setDirection(direction);
+//                     car.setSize(size);
+//                     car.setId(i);
+//                     grid.addCar(car);
+//                     count++;
+
+//                     for (int k = 0; k < size; k++) {
+//                         grid.setGridCarId(x, y + k, i);
+//                         line.erase(find(line.begin(), line.end(), y + k));
+//                     }
+
+//                     putCar = true;
+//                 }
+//             }
+//         }
+//         else if(direction == VERTICAL){
+//             int x;
+//             int y = rand()%6;
+//             int size = rand() % 2 + 2;
+//             for (int xx = 0; xx <= HEIGHT; xx++) { // Check for available spaces in the column
+//                 bool validSpace = false;
+//                 for (int k = 0; k < size; k++) {
+
+//                     cout << "x : " << xx+k << ", y : " << y << ", k : " << k  << endl;
+//                     cout << "Grid value at " << xx+k << ", " << y  << " : " << gridCarId[xx + k][y] << endl << endl ;
+
+//                     if (gridCarId[xx + k][y] != -1) {
+//                         validSpace = true;
+//                         break;
+//                     }
+//                 }
+//                 if (!validSpace) {
+//                     line.push_back(xx);
+//                 }
+//             }
+
+//             if(!line.empty()){ // Check if there are any valid spaces in the column
+//                 int n = rand() % line.size();
+//                 x = line[n];
+//                 size = rand() % 2 + 2;
+
+//                 // Check if car is inside the grid
+//                 if(x >= 0 && x + size - 1 < HEIGHT && y >= 0 && y < WIDTH) {
+//                     car.setPosX(x);
+//                     car.setPosY(y);
+//                     car.setDirection(direction);
+//                     car.setSize(size);
+//                     car.setId(i);
+//                     grid.addCar(car);
+//                     count++;
+
+//                     for (int k = 0; k < size; k++) {
+//                         grid.setGridCarId(x + k, y, i);
+//                         line.erase(find(line.begin(), line.end(), x + k));
+//                     }
+
+//                     putCar = true;
+//                 }
+//             }
+//         }
+//         if(!putCar){ // If the car was not put, decrement i to try again
+//             i--;
+//         }
+//     }
+//     cout << "dcount" << count << endl;
+// }
+
+
+Grid Puzzle::generateRandomPuzzle(){
+    
+    makeEmptyGrid();
+
+    grid.displayGridId(); //test ok
+
+    int gridCarId[6][6];
+    grid.getGridCarId(gridCarId);
+    randCar2(gridCarId);
+
+    grid.displayGridId();
+
+
+        // on rajoute le cas initial
+        ofstream file("./puzzle/.svg");
+        file << grid.svgHeader() << grid.svgRectangle() << grid.svgFooter(); 
+
+
+
+    
+    return grid;
+
 }
